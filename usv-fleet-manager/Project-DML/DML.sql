d61e9f66333e2d1a9ff6ff9b6eb8fec36cd3b173
quetzlcoatlus <lnalex2456@gmail.com>
2026-02-10 21:51:18 -0800

    added some unsaved changes

--- usv-fleet-manager/Project-DML/DML.sql
+++ usv-fleet-manager/Project-DML/DML.sql
@@ -150,6 +150,17 @@ INSERT INTO Payloads (
 );
 
 -- Update an existing Payload
+UPDATE Payloads SET
+    Payloads.type = :type_input, 
+    Payloads.serialNumber = :serial_number_input, 
+    Payloads.condition = :condition_from_dropdown,
+    -- Payloads.installedUSV = (SELECT missionID FROM Missions WHERE Missions.title = :mission_title_from_dropdown),
+    Payloads.installedUSV = :mission_id_from_dropdown
+    -- Can be NULL
+    
+    Payloads.installationDate = :installation_date_input
+    -- Can be NULL
+WHERE Payloads.payloadID = :payload_id_selected;
 
 -- Get all USVs to populate the "Installed On" dropdown
 SELECT 
--- usv-fleet-manager/Project-DML/Payloads_page/Payload_update.sql
+++ usv-fleet-manager/Project-DML/Payloads_page/Payload_update.sql
@@ -6,7 +6,7 @@ UPDATE Payloads SET
     -- Payloads.installedUSV = (SELECT missionID FROM Missions WHERE Missions.title = :mission_title_from_dropdown),
     Payloads.installedUSV = :mission_id_from_dropdown
     -- Can be NULL
-    
+
     Payloads.installationDate = :installation_date_input
     -- Can be NULL
 WHERE Payloads.payloadID = :payload_id_selected;
